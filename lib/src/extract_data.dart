a929607c38c538cb0b1bb50f6a4942aeaded2f9a
r-yeh <r.yeh2008@gmail.com>
2024-01-07 18:28:34 -0500

    added method to get string from extracted data

--- lib/src/extract_data.dart
+++ lib/src/extract_data.dart
@@ -1,5 +1,17 @@
 import 'dart:io';
 
 class ExtractData {
+  // List<String> stringToList(String data) {
+  //   List<String> splitString = data.split('\n');
+  //
+  //   return splitString;
+  // }
 
+  String getDate(String receiptData) {
+    final dateRegex = RegExp(
+        r'/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/');
+    RegExpMatch? date = dateRegex.firstMatch(receiptData);
+
+    return date.toString();
+  }
 }
