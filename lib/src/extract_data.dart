5d2896044e27d22d3a2b3fedd7c9b027223e0fd4
r-yeh <r.yeh2008@gmail.com>
2024-01-08 17:52:17 -0500

    changed date regex to datetime regex

--- lib/src/extract_data.dart
+++ lib/src/extract_data.dart
@@ -1,10 +1,10 @@
 class ExtractData {
-  // extract date from the input receipt text if it exists
-  String? getDate(String receiptData) {
-    final dateRegex = RegExp(
-        r'^(\d{1,2})[/\-](\d{1,2})[/\-](\d{4})');
-    String? date = dateRegex.stringMatch(receiptData);
+  // extract date and time from the input receipt text if it exists
+  String? getDateTime(String receiptData) {
+    final dateTimeRegex = RegExp(
+        r'(\d{1,2})[/\-](\d{1,2})[/\-](\d{4}) ([0-1]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?');
+    String? dateTime = dateTimeRegex.stringMatch(receiptData);
 
-    return date;
+    return dateTime;
   }
 }
