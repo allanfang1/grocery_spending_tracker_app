be3993e3f4c458aaae59f8c796ab73ad00c3f08d
r-yeh <r.yeh2008@gmail.com>
2024-01-09 17:53:32 -0500

    refactored existing functions to use List<String> and added getItems

--- lib/src/extract_data.dart
+++ lib/src/extract_data.dart
@@ -1,21 +1,50 @@
 class ExtractData {
-  // extract date and time from the input receipt text if it exists
-  String? getDateTime(String receiptData) {
-    final dateTimeRegex = RegExp(
-        r'(\d{1,2})[/\-](\d{1,2})[/\-](\d{4}) ([0-1]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?');
-    String? dateTime = dateTimeRegex.stringMatch(receiptData);
+  List<String> textToList(String receiptData) {
+    List<String> splitReceipt = receiptData.split('\n');
 
-    return dateTime;
+    for (int i = 0; i < splitReceipt.length; i++) {
+      String singleLine = splitReceipt[i].replaceAll('\n', '');
+      splitReceipt[i] = singleLine;
+    }
+
+    return splitReceipt;
   }
 
-  String? getLocation(String receiptData) {
+  // extract date and time from the input receipt text if it exists
+  String? getDateTime(List<String> receiptData) {
+    final dateTimeRegex = RegExp(
+        r'(\d{1,2})[/\-](\d{1,2})[/\-](\d{4}) ([0-1]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?');
+
+    for (String line in receiptData) {
+      if (dateTimeRegex.hasMatch(line)) return line;
+    }
+
+    return null;
+  }
+
+  String? getLocation(List<String> receiptData) {
     // Full Street Address, City, Province, Postal Code
-    // r'(\d+) [a-zA-Z\s]+(\,)? [a-zA-Z]+(\,)? ([A-Z]{2})+(\,)? [A-Z0-9\s]{6,7}'
+    // r'([1-9]+) [a-zA-Z\s]+(\,)? [a-zA-Z]+(\,)? ([A-Z]{2})+(\,)? [A-Z0-9\s]{6,7}'
 
     // want to add optional city, province and postal
-    final addressRegex = RegExp(r'(\d+) [a-zA-Z\s]');
-    String? address = addressRegex.stringMatch(receiptData);
+    final addressRegex = RegExp(r'([1-9]+) [a-zA-Z\s]');
 
-    return address;
+    for (String line in receiptData) {
+      if (addressRegex.hasMatch(line)) return line;
+    }
+
+    return null;
+  }
+
+  List<String> getItems(List<String> receiptData) {
+    List<String> groceries = [];
+
+    final itemRegex = RegExp(r'([a-zA-Z\s]+) [$]?[0-9]+.[0-9]{2}');
+
+    for (String line in receiptData) {
+      if (itemRegex.hasMatch(line)) groceries.add(line);
+    }
+
+    return groceries;
   }
 }
