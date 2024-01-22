8279b8172fe5d1d7b7a7612b72f986f7a8175abb
r-yeh <r.yeh2008@gmail.com>
2024-01-22 12:50:10 -0500

    removed irrelevant comments

--- lib/src/confirm_receipt.dart
+++ lib/src/confirm_receipt.dart
@@ -37,9 +37,4 @@ class _ConfirmReceiptState extends State<ConfirmReceipt> {
           child: Text("Hello"),
         ),
       );
-
-  // maybe have to move these to a different file to perform the
-  // computations earlier
-
-
 }
