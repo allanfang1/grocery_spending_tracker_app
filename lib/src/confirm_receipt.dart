3ea21e684690981ef97781eaa98ca013bc11f253
r-yeh <r.yeh2008@gmail.com>
2024-01-23 16:27:12 -0500

    started form formatting for receipt confirmation

--- lib/src/confirm_receipt.dart
+++ lib/src/confirm_receipt.dart
@@ -5,36 +5,71 @@ import 'package:grocery_spending_tracker_app/src/item.dart';
 class ConfirmReceipt extends StatefulWidget {
   final GroceryTrip tripData;
 
-  const ConfirmReceipt({ Key? key, required this.tripData}): super(key: key);
+  const ConfirmReceipt({Key? key, required this.tripData}) : super(key: key);
 
   @override
   State<ConfirmReceipt> createState() => _ConfirmReceiptState();
 }
 
 class _ConfirmReceiptState extends State<ConfirmReceipt> {
-  late int dateTime;
-  late String location;
-  late List<Item> items;
-  late double subtotal;
-  late double total;
-  late String? tripDesc;
+  late int _dateTime;
+  late String _location;
+  late List<Item> _items;
+  late double _subtotal;
+  late double _total;
+  late String? _tripDesc;
+
+  final _confirmReceiptKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     super.initState();
 
-    dateTime = widget.tripData.dateTime;
-    location = widget.tripData.location;
-    items = widget.tripData.items;
-    subtotal = widget.tripData.subtotal;
-    total = widget.tripData.total;
-    tripDesc = widget.tripData.tripDesc;
+    _dateTime = widget.tripData.dateTime;
+    _location = widget.tripData.location;
+    _items = widget.tripData.items;
+    _subtotal = widget.tripData.subtotal;
+    _total = widget.tripData.total;
+    _tripDesc = widget.tripData.tripDesc;
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
+  }
+
+  Widget _buildDateTime() {
+    return Text("hello");
+  }
+
+  Widget _buildLocation() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Location'),
+      initialValue: _location,
+      validator: (String? value) {
+        if (value == null || value.isEmpty) return 'Location is required';
+        return null;
+      },
+      onSaved: (String? value) {
+        _location = value!;
+      },
+    );
+  }
+
+  Widget _buildItems() {
+    return Text("hello");
+  }
+
+  Widget _buildSubtotal() {
+    return Text("hello");
+  }
+
+  Widget _buildTotal() {
+    return Text("hello");
+  }
+
+  Widget _buildTripDesc() {
+    return Text("hello");
   }
 
   @override
@@ -43,8 +78,31 @@ class _ConfirmReceiptState extends State<ConfirmReceipt> {
           title: const Text('Confirm Scanned Receipt'),
         ),
         body: Container(
-          padding: const EdgeInsets.all(30.0),
-          child: Text("Hello"),
+          margin: const EdgeInsets.all(24.0),
+          child: Form(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // _buildDateTime(),
+              _buildLocation(),
+              // _buildItems(),
+              // _buildSubtotal(),
+              // _buildTotal(),
+              // _buildTripDesc(),
+              SizedBox(height: 100),
+              ElevatedButton(
+                  onPressed: () => {
+                        if (_confirmReceiptKey.currentState!.validate())
+                          {
+                            // TODO Save Data, Update Grocery Trip, Send Data
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data')),
+                            )
+                          }
+                      },
+                  child: const Text('Confirm Receipt'))
+            ],
+          )),
         ),
       );
 }
