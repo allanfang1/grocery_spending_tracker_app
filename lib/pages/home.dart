f8d236b98ea269aa9be3d5208536d64befd419ff
r-yeh <r.yeh2008@gmail.com>
2024-02-21 10:42:17 -0500

    filled out missing parts of home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,50 +11,62 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final getUser = ref.watch(profileControllerProvider);
-    final user = getUser.when(
-      data: (user) => user,
-      loading: () => null, // Handle loading state (e.g., show a spinner)
-      error: (error, stackTrace) {
-        // Handle error state (e.g., show an error message)
-        print('Error fetching user: $error');
-        return null;
-      },
-    );
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(Constants.APP_NAME),
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 300.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                  padding: EdgeInsets.only(top: 20.0),
-                  child: Text(
-                    'Welcome ${user?.firstName} ${user?.lastName}!',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                    textAlign: TextAlign.center,
-                  )),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('logout moved to profile page',
-                        style: TextStyle(fontSize: 18))
-                  ],
-                ),
-              ),
-              // TODO: Set up a swipeable list to use for recommendation
-            ],
+    return getUser.when(data: (user) {
+        final user = ref.watch(profileControllerProvider.notifier).getUser();
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(Constants.APP_NAME),
+            automaticallyImplyLeading: false,
           ),
-        ),
-      ),
+          body: Center(
+            child: Container(
+              constraints: const BoxConstraints(maxWidth: 300.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Text(
+                        'Welcome ${user.firstName} ${user.lastName}!',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                        textAlign: TextAlign.center,
+                      )),
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('logout moved to profile page',
+                            style: TextStyle(fontSize: 18))
+                      ],
+                    ),
+                  ),
+                  // TODO: Set up a swipeable list to use for recommendation
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      loading: () {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(Constants.APP_NAME),
+          ),
+          body: Center(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(24, 0, 24, 0),
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return Text('Error fetching user: $error');
+      },
     );
   }
 }
